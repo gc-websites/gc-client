b42566a1b96ca7ccc494ec23cc51af08c6c9e2e2
Назар Івченко <nazary9911@gmail.com>
2025-12-02 06:39:38 +0200

    feature/tags

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -3,11 +3,10 @@ import React, { useEffect, useState } from 'react';
 const Product = () => {
   const params = new URLSearchParams(window.location.search);
   const fbclid = params.get('fbclid');
-  const url = window.location.href;
-  const id = url.split('/').pop();
+  const url = new URL(window.location.href);
+  const pathname = url.pathname;
+  const id = pathname.split('/').pop();
   const [productData, setProductData] = useState({});
-  const [clickId, setClickId] = useState('');
-  const [urlWithSubtag, setUrlWithSubtag] = useState('');
 
   useEffect(() => {
     try {
@@ -30,44 +29,73 @@ const Product = () => {
   }, []);
 
   useEffect(() => {
-    if (fbclid) {
-      try {
-        fetch(`https://dev.nice-advice.info/get-product/ads/${id}`, {
-          headers: { 'Content-Type': 'application/json' },
-          method: 'POST',
-          body: JSON.stringify({ fbclid: fbclid }),
-        })
-          .then(res => {
-            if (!res.ok) {
-              throw new Error('Failed to fetch products');
-            }
-            return res.json();
-          })
-          .then(data => {
-            setClickId(data.data);
+    if (fbclid && id && productData.tag) {
+      const send = async () => {
+        try {
+          const res = await fetch(`https://dev.nice-advice.info/fbclid`, {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'POST',
+            body: JSON.stringify({
+              fbclid,
+              productId: id,
+              tag: productData.tag,
+            }),
           });
-      } catch (err) {
-        console.error('❌ Error:', err);
-      }
-    }
-  }, []);
 
-  useEffect(() => {
-    if (productData && clickId) {
-      setUrlWithSubtag(productData?.link + `&ascsubtag=${clickId}`);
-      console.log(urlWithSubtag);
-    } else {
-      setUrlWithSubtag(productData?.link);
-      console.log(urlWithSubtag);
+          if (!res.ok) {
+            throw new Error('Failed to send fbclid');
+          }
+
+          const data = await res.json(); // ← ЖДЁМ
+          console.log(data);
+        } catch (err) {
+          console.error('❌ Error:', err);
+        }
+      };
+
+      send();
     }
-  }, [clickId, productData]);
+  }, [productData.tag]);
+
+  // useEffect(() => {
+  //   if (fbclid) {
+  //     try {
+  //       fetch(`https://dev.nice-advice.info/get-product/ads/${id}`, {
+  //         headers: { 'Content-Type': 'application/json' },
+  //         method: 'POST',
+  //         body: JSON.stringify({ fbclid: fbclid }),
+  //       })
+  //         .then(res => {
+  //           if (!res.ok) {
+  //             throw new Error('Failed to fetch products');
+  //           }
+  //           return res.json();
+  //         })
+  //         .then(data => {
+  //           setClickId(data.data);
+  //         });
+  //     } catch (err) {
+  //       console.error('❌ Error:', err);
+  //     }
+  //   }
+  // }, []);
+
+  // useEffect(() => {
+  //   if (productData && clickId) {
+  //     setUrlWithSubtag(productData?.link);
+  //     console.log(urlWithSubtag);
+  //   } else {
+  //     setUrlWithSubtag(productData?.link);
+  //     console.log(urlWithSubtag);
+  //   }
+  // }, [clickId, productData]);
 
   return (
     <div className="flex flex-col justify-center items-center p-5">
       <img
         id="cta-image"
         src={productData?.image?.url}
-        onClick={() => window.open(urlWithSubtag, '_blank')}
+        onClick={() => window.open(productData?.link, '_blank')}
         className="w-[90vw] md:w-[40vw] rounded-xl cursor-pointer transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:shadow-xl"
       />
       <h1 className="text-2xl md:text-3xl text-center font-bold w-[90%]">
@@ -81,7 +109,7 @@ const Product = () => {
       <p className="w-[90%] md:w-[40%]">{productData?.descriptionfield3}</p>
       <p className="w-[90%] md:w-[40%]">{productData?.descriptionfield4}</p>
       <a
-        href={urlWithSubtag}
+        href={productData?.link}
         target="_blank"
         rel="noopener noreferrer"
         className="w-[90vw] md:w-[40vw] p-5 m-5 rounded bg-[rgb(3,145,133)] text-2xl font-bold relative bg-[#eaa31e] border border-black rounded-lg py-[15px] px-[20px] mb-[10px] flex justify-center items-center cursor-pointer transition-colors duration-300 font-bold overflow-hidden text-[17px] hover:bg-[#c47f00]
